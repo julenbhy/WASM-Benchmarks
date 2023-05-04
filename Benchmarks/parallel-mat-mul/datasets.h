b1c971769a5c3605bcd72f71936b232f93986af9
julenbhy <julenbhy@gmail.com>
2023-05-04 20:58:59 +0200

    big changes

---
+++ Benchmarks/parallel-mat-mul/datasets.h
@@ -0,0 +1,18 @@
+#ifdef SMALL_DATASET
+#define SIZE 4
+#endif
+#ifdef LARGE_DATASET
+#define SIZE 100
+#endif
+
+#ifndef SMALL_DATASET
+  #ifndef LARGE_DATASET
+    #ifndef MEDIUM_DATASET
+      #define MEDIUM_DATASET
+    #endif
+  #endif
+#endif
+
+#ifdef MEDIUM_DATASET
+#define SIZE 10
+#endif
